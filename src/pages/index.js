67010151b28506347bef2b9a0c134441b2ff75ab
Will Hall <accounts@willhall.uk>
2020-06-15 18:35:12 +0100

    🎨 Tidy up repo to most basic state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Under Construction" />
     <h1>STARLINK.CAM</h1>
     <p>An epic visual of the SpaceX Starlink Constellation.</p>
     <p>Under construction my dudes.</p>
-  </Layout>
+  </>
 )
 
 export default IndexPage
